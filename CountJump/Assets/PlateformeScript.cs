a92554360167c1eac9175618e202179bf92f2cf9
Régis <regis.poizat@cegetel.net>
2020-04-03 15:43:17 +0200

    push du projet

---
+++ CountJump/Assets/PlateformeScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateformeScript : MonoBehaviour
+{
+    [SerializeField] private Transform PFtransform;
+
+    //changer les dimensions de la plateforme
+    public void RescalePlateforme(float scaleX, float scaleY)
+    {
+        PFtransform.localScale = new Vector3(scaleX, scaleY, 2);
+    }
+
+    //placer la plateforme
+    public void Reposition(Vector3 position)
+    {
+        PFtransform.position = position;
+    }
+}
---
+++ CountJump/Assets/PlayerControleurScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerControleurScript : MonoBehaviour
+{
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private Rigidbody playerRigidbody;
+    [SerializeField] private float playerSpeed;
+    [SerializeField] private float playerJump;
+
+    private Ray scanJump;
+    private Vector3 posRay;
+    private bool canJump = true;
+
+    public void ReplacePlayer(Vector3 position)
+    {
+        playerTransform.position = position;
+    }
+
+    private void FixedUpdate()
+    {
+        posRay = new Vector3(playerTransform.position.x, playerTransform.position.y - 0.5f, playerTransform.position.z);
+
+        if (Physics.Raycast(posRay, -playerTransform.up, 0.5f) && canJump == false)
+        {
+            canJump = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //avancer vers la droite
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            playerTransform.Translate(playerTransform.right * playerSpeed * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            playerTransform.Translate(-playerTransform.right * playerSpeed * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.UpArrow) && canJump)
+        {
+            playerRigidbody.AddForce(playerTransform.up * playerJump, ForceMode.Acceleration);
+            canJump = false;
+        }
+    }
+}
